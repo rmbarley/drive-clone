bbe818dbf4bfbbc7d981f9d64a199327ad4dcc58
ryanbarley <ryan.barley@whitespace.us>
2025-02-03 21:26:27 -0500

    Separate Folders from Files

---
+++ src/app/FileRow.tsx
@@ -0,0 +1,58 @@
+import { FileIcon, Folder as FolderIcon } from "lucide-react";
+import React from "react";
+import type { Folder, File } from "~/lib/mock-data";
+
+const FileRow = ({ file }: { file: File }) => {
+  return (
+    <li
+      key={file.id}
+      className="hover:bg-gray-750 border-b border-gray-700 px-6 py-4"
+    >
+      <div className="grid grid-cols-12 items-center gap-4">
+        <div className="col-span-6 flex items-center">
+          <a
+            href={file.url}
+            target="_blank"
+            className="flex items-center text-gray-100 hover:text-blue-400"
+          >
+            <FileIcon className="mr-3" size={20} />
+            {file.name}
+          </a>
+        </div>
+        <div className="col-span-3 text-gray-400">File</div>
+        <div className="col-span-3 text-gray-400">{file.size}</div>
+      </div>
+    </li>
+  );
+};
+
+const FolderRow = ({
+  folder,
+  handleFolderClick,
+}: {
+  folder: Folder;
+  handleFolderClick: () => void;
+}) => {
+  return (
+    <li
+      key={folder.id}
+      className="hover:bg-gray-750 border-b border-gray-700 px-6 py-4"
+    >
+      <div className="grid grid-cols-12 items-center gap-4">
+        <div className="col-span-6 flex items-center">
+          <button
+            onClick={() => handleFolderClick()}
+            className="flex items-center text-gray-100 hover:text-blue-400"
+          >
+            <FolderIcon className="mr-3" size={20} />
+            {folder.name}
+          </button>
+        </div>
+        <div className="col-span-3 text-gray-400">Folder</div>
+        <div className="col-span-3 text-gray-400">--</div>
+      </div>
+    </li>
+  );
+};
+
+export { FileRow, FolderRow };
