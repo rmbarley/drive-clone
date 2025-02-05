870621b4c8c0e418191368d412cbad477ca0101a
ryanbarley <rmbarley@gmail.com>
2025-02-04 21:30:23 -0500

    Setup folder based routing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,3 @@
-import {
-  files as filesSchema,
-  folders as foldersSchema,
-} from "~/server/db/schema";
-import DriveContents from "./drive-contents";
-import { db } from "~/server/db";
-
 export default async function GoogleDriveClone() {
-  const files = await db.select().from(filesSchema);
-  const folders = await db.select().from(foldersSchema);
-
-  return <DriveContents files={files} folders={folders} />;
+  return <div>Homepage</div>;
 }
